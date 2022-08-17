9291fb89989af9f7dbc58d92aefad492c8854600
Yuvraj Paliwal <78093228+yuvraj-xyz@users.noreply.github.com>
2022-08-16 23:07:15 -0600

    Update Power of 2.cpp

--- Power of 2.cpp
+++ Power of 2.cpp
@@ -1,3 +1,28 @@
+// class Solution {
+// public:
+//     bool isPowerOfTwo(int n) {
+//         if(n==0)
+//         {
+//             return false;
+//         }
+//         if(n==1)
+//         {
+//             return true;
+//         }
+//         while(n%2==0)
+//         {
+//             n/=2;
+//             if(n==1)
+//             {
+//                 return true;
+//                 break;
+//             }
+//         }
+//         return false;
+//     }
+// };
+
+// RECURSION
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
@@ -21,3 +46,4 @@ public:
         return false;
     }
 };
+
