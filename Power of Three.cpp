eff0351e9df1fc5c13523e1850d3fe209e047ff8
Yuvraj Paliwal <78093228+yuvraj-xyz@users.noreply.github.com>
2022-08-17 02:59:09 -0600

    Create Power of Three.cpp

---
+++ Power of Three.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n==1 || n==3)
+        {
+            return true;
+        }
+        else if(n%3!=0 || n==0)
+        {
+            return false;
+        }
+        else
+        {
+            return isPowerOfThree(n/3);
+        }
+        return false;
+    }
+};
